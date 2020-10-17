50f1ddb8769ce2c56a567c97bb69a130ad8155bf
manojbatham <manoj.batham@gmail.com>
2020-10-17 12:25:02 +0530

    added the solution

---
+++ SuperHeroDB/SuperHeroDB/Service/SuperHeroService.svc.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace SuperHeroDB.Service
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class SuperHeroService
+    {
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public string DoWork()
+        {
+            return "This is the SuperHeroDB service!";
+        }
+    }
+}
